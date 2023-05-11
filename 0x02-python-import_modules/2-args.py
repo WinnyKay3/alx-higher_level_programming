a233d8092f1c95d15f8471f6085400a278f383e0
WinnyKay3 <winffredkalondu@gmail.com>
2023-05-11 11:11:31 -0700

    2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
 
-    import sys
+    from sys import argv
 
-    le = sys.argv
-    size = len(le) -1
+    size = len(argv) - 1
     ar = "argument" if size == 1 else "arguments"
     dot = "." if size == 0 else ":"
     print("{} {}".format(size, ar + dot))
     for i in range(1, size + 1):
-        print("{}: {}".format(1, le[i]))
+        print("{}: {}".format(i, argv[i]))
