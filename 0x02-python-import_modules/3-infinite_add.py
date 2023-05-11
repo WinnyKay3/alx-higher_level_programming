5e695cb88b000e81491d53f51c215df275f43a13
WinnyKay3 <winffredkalondu@gmail.com>
2023-05-11 10:16:43 -0700

    3-infinite_add.py

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+
+if __name__ == "__main__":
+
+    import sys
+
+    le = sys.argv
+    sum = 0
+    for i in range(1, len(le)):
+        sum = sum + int(le[i])
+    print("{}".format(sum))
