ef36c3f3f4b1711a94e98e562ddd2265bb0576ab
WinnyKay3 <winffredkalondu@gmail.com>
2023-05-13 12:35:18 -0700

    102-magic_calculation.py

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -9,5 +9,5 @@ def magic_calculation(a, b):
         for t in range(4, 6):
             c = add(c, t)
             return c
-        else:
-            return sub(a, b)
+    else:
+        return sub(a, b)
