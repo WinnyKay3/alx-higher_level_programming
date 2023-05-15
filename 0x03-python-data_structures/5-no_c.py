d32e34744d4f42be67772d27603619763727687d
WinnyKay3 <winffredkalondu@gmail.com>
2023-05-15 04:44:16 -0700

    5-no_c.py

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -2,6 +2,6 @@
 def no_c(my_str):
     new_str = ""
     for char in my_str:
-        if char != 'c' and char != 'C':
+        if char != "c" and char != "C":
             new_str += char
     return new_str
