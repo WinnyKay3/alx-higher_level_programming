eedc95fc877fd293dfbc8b6ac6df06534e6435ef
WinnyKay3 <winffredkalondu@gmail.com>
2023-05-30 03:52:31 -0700

    1-square.py

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -2,14 +2,15 @@
 
 """Define a class Square."""
 
+
 class Square:
     """Represent a square."""
 
     def __init__(self, size=0):
         """Initialize a new Square.
-    
-    Args:
-        size (int): The size of the new square
+
+        Args:
+            size (int): The size of the new square
         """
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
