684f04c73abdaca0c4a7a226ed29727a3f158347
WinnyKay3 <winffredkalondu@gmail.com>
2023-06-12 10:59:25 -0700

    1-my_list.py

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,14 +1,22 @@
 #!/usr/bin/python3
-
-"""Defines an inherited list class Mylist."""
+"""
+This module shows a class MyList tha inherit from
+the well known list class
+so it will inherit all the public methods and attributes such as append
+It also inherits the sort methos
+"""
 
 
 class MyList(list):
-    """Implements sorted printing for the built-in list class."""
-
+    """
+    a blueprint of MyList a subclass of list
+    that inherits all the things of list
+    it also adds its own method print_sorted()
+    """
     def print_sorted(self):
-        """Print the list in a sorted way, ascending order"""
-
+        """
+        print the list in a sorted way, ascending order
+        """
         list_t = self[:]
         list_t.sort()
         print(list_t)
