1d84102b77df31a488eb5c335cfe53ae6eb8f35d
WinnyKay3 <winffredkalondu@gmail.com>
2023-06-19 19:15:29 -0700

    update square.py file

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -1,61 +1,81 @@
 #!/usr/bin/python3
-"""
-This module contains the class square that inherits from
-the class Rectangle
-"""
+"""Describe a class square"""
+
+
 from models.rectangle import Rectangle
 
 
 class Square(Rectangle):
-    """
-    class square inheriting from rectangle
-    """
+    """Square class that inherits from the rect class
+
+    Attr:
+        size (int): size of square
+        x(int): x coordinate of de square's position
+        y(int): y coordinate of de square's position"""
 
     def __init__(self, size, x=0, y=0, id=None):
-        """
-        class constructure
-        """
+        """Initializes new instance of the square class"""
+
         super().__init__(size, size, x, y, id)
+
+    def update(self, *args, **kwargs):
+        """
+        Assigns attributes to the Square instance based on *args and **kwargs.
+
+        Args:
+            *args: List of arguments.
+            **kwargs: Dictionary of keyword arguments.
+        """
+        if args:
+            if len(args) > 0:
+                self.id = args[0]
+            if len(args) > 1:
+                self.size = args[1]
+            if len(args) > 2:
+                self.x = args[2]
+            if len(args) > 3:
+                self.y = args[3]
+        else:
+            if "id" in kwargs:
+                self.id = kwargs["id"]
+            if "size" in kwargs:
+                self.size = kwargs["size"]
+            if "x" in kwargs:
+                self.x = kwargs["x"]
+            if "y" in kwargs:
+                self.y = kwargs["y"]
 
     def __str__(self):
         """
-        string representation of the square
+        Returns a string representation of the Square instance.
+
+        Returns:
+            str: The string representation of the square.
         """
-        return "[Square] ({}) {}/{} - {}".format(self.id, self.x, self.y, self.width)
+        return f"[Square] ({self.id}) {self.x}/{self.y} - {self.width}"
+
+    def to_dictionary(self):
+        """
+        Returns the dictionary representation of a Rectangle.
+
+        Returns:
+            dict: The dictionary representation of the rectangle.
+        """
+        return {
+            "id": self.id,
+            "width": self.width,
+            "height": self.height,
+            "x": self.x,
+            "y": self.y,
+        }
 
     @property
     def size(self):
-        """
-        get the value of the size
-        """
+        """Retrieve the size of the square."""
         return self.width
 
     @size.setter
     def size(self, value):
-        """
-        set the value of the size
-        """
+        """Set the size of the square."""
         self.width = value
         self.height = value
-
-    def update(self, *args, **kwargs):
-        """
-        update the square with keyword arguments
-        """
-        attributes = ["id", "size", "x", "y"]
-
-        for idx, x in enumerate(args):
-            if idx >= len(attributes):
-                return self.__setattr__(attributes[idx], x)
-
-        if args:
-            return
-
-        for k, v in kwargs.items():
-            self.__setattr__(k, v)
-
-    def to_dictionary(self):
-        """
-        dictionary representation of a square
-        """
-        return {"id": self.id, "size": self.size, "x": self.x, "y": self.y}
