fa73c83a0fea0d412b67aac271da01673d472fa0
WinnyKay3 <winffredkalondu@gmail.com>
2023-07-19 12:32:03 -0700

    5-unique_id.sql

--- 0x0E-SQL_more_queries/5-unique_id.sql
+++ 0x0E-SQL_more_queries/5-unique_id.sql
@@ -1,7 +1,4 @@
 -- Creates the table unique_id on MySQL.
 CREATE TABLE IF NOT EXISTS unique_id(
-    id INT, DEFAULT 1 UNIQUE, name VARCHAR(256)
+    id INT DEFAULT 1 UNIQUE, name VARCHAR(256)
 );
-
--- flush the privilages to include changes
-FLUSH PRIVILAGES;
