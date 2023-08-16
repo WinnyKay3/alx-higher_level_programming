3d86aa8d9b41c7aa9684f140764abe5afcbbd2bf
WinnyKay3 <winffredkalondu@gmail.com>
2023-08-16 11:15:37 -0700

    fix codestyle

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -1,18 +1,18 @@
 #!/usr/bin/node
 module.exports = class Rectangle {
   constructor (w, h) {
-    if (w > 0 && h > 0) {[this.width, this.height] = [w, h]};
-}
+    if (w > 0 && h > 0) { [this.width, this.height] = [w, h]; }
+  }
 
-print () {
-  for (let k = 0; k < this.height; k++) console.log('X'.repeat(this.width));
-}
+  print () {
+    for (let k = 0; k < this.height; k++) console.log('X'.repeat(this.width));
+  }
 
-rotate () {
-  [this.width, this.height] = [this.width, this.height]
-}
+  rotate () {
+    [this.height, this.width] = [this.width, this.height];
+  }
 
-double () {
-  [this.width, this.height] = [this.width *2, this.height *2]
+  double () {
+    [this.width, this.height] = [this.width * 2, this.height * 2];
   }
 };
