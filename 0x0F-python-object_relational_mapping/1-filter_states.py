1db25d654cfd3f4a9c3edb4429eb74ad3239f090
WinnyKay3 <winffredkalondu@gmail.com>
2023-08-22 03:50:07 -0700

    5-filter_cities.py

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,23 +1,19 @@
 #!/usr/bin/python3
-'''
+"""
 List states with name starting
 with N, from hbtn_0e_usa database.
-'''
+"""
 
 import MySQLdb
 from sys import argv
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     db = MySQLdb.connect(
-        host='localhost',
-        port=3306,
-        user=argv[1],
-        passwd=argv[2],
-        db=argv[3])
+        host="localhost", port=3306, user=argv[1], passwd=argv[2], db=argv[3]
+    )
     cur = db.cursor()
-    cur.execute(
-        "SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC;")
+    cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC;")
     for row in cur.fetchall():
-        if row[1][0] == 'N':
+        if row[1][0] == "N":
             print(row)
     db.close()
