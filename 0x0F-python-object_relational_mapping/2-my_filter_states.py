194ce989afea6fdb3b75c4af7935900a4511338c
WinnyKay3 <winffredkalondu@gmail.com>
2023-08-22 10:27:42 +0000

    5-filter_cities.py

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,21 +1,22 @@
 #!/usr/bin/python3
-''' Takes an argument and displays all values in the states table
-     of hbtn_0e_0_usa where name matches the argument'''
+""" Takes an argument and displays all values in the states table
+     of hbtn_0e_0_usa where name matches the argument"""
 
 import MySQLdb
 from sys import argv
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(host="localhost",
-                         port=3306,
-                         user=argv[1],
-                         password=argv[2],
-                         database=argv[3])
+    db = MySQLdb.connect(
+        host="localhost", port=3306, user=argv[1], password=argv[2], database=argv[3]
+    )
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name LIKE\
-    '{:s}' ORDER BY id ASC".format(argv[4]))
+    cur.execute(
+        "SELECT * FROM states WHERE name LIKE\
+    '{:s}' ORDER BY id ASC".format(
+            argv[4]
+        )
+    )
     for data in cur.fetchall():
         if data[1] == argv[4]:
             print(data)
     db.close()
-
--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,20 +1,21 @@
 #!/usr/bin/python3
-'''
+"""
 A script that is free from MySQL Injection.
-'''
+"""
 
 import MySQLdb
 from sys import argv
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(host="localhost",
-                         port=3306,
-                         user=argv[1],
-                         password=argv[2],
-                         database=argv[3])
+    db = MySQLdb.connect(
+        host="localhost", port=3306, user=argv[1], password=argv[2], database=argv[3]
+    )
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name LIKE\
-    %(state_name)s ORDER BY id ASC", {'state_name': argv[4]})
+    cur.execute(
+        "SELECT * FROM states WHERE name LIKE\
+    %(state_name)s ORDER BY id ASC",
+        {"state_name": argv[4]},
+    )
     for data in cur.fetchall():
         if data[1] == argv[4]:
             print(data)
