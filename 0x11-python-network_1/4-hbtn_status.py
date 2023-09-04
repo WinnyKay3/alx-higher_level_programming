a3577d2d6ce68d071e7702396a96fbf8a7227fc4
WinnyKay3 <winffredkalondu@gmail.com>
2023-09-04 11:38:49 +0000

    4-hbtn_status.py

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -3,7 +3,7 @@
 import requests
 
 if __name__ == "__main__":
-    k = requests.get("https://intranet.hbtn.io/status")
+    k = requests.get("https://alx-intranet.hbtn.io/status")
     print("Body response:")
     print("\t- type: {}".format(type(k.text)))
     print("\t- content: {}".format(k.text))
