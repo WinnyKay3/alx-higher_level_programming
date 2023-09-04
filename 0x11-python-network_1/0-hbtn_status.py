146419c45b90586105a4bf505936269242806f1c
WinnyKay3 <winffredkalondu@gmail.com>
2023-09-04 11:47:35 +0000

    0-hbtn_status.py

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
 """Fetches https://intranet.hbtn.io/status."""
-from urllib import request
 
+from urllib import request
 
 if __name__ == "__main__":
     url = "https://alx-intranet.hbtn.io/status"
+
     with request.urlopen(url) as response:
-        body = response.read()
+        response_body = response.read()
+
         print("Body response:")
         print("\t- type: {}".format(type(response_body)))
         print("\t- content: {}".format(response_body))
