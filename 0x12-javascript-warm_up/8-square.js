01d26c9ef71ab17405b4a9dc42888dd1c9ccf24e
WinnyKay3 <winffredkalondu@gmail.com>
2023-09-11 17:04:42 +0000

    8-square.js

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -7,13 +7,13 @@ const size = parseInt(firstArg);
 if (!isNaN(size)) {
   if (size > 0) {
     for (let k = 0; k < size; k++) {
-       let row = '';
-       for (let m = 0; m < size; m++) {
-	   row += 'X';
-       }
-       console.log(row);
-     }
-   }
- } else {
-   console.log('Missing size');
+      let row = '';
+      for (let m = 0; m < size; m++) {
+        row += 'X';
+      }
+      console.log(row);
+    }
+  }
+} else {
+  console.log('Missing size');
 }
