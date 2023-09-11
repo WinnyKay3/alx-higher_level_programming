1a80acfcfb307b9548f74182ff4bcb893dc486c3
WinnyKay3 <winffredkalondu@gmail.com>
2023-09-11 16:16:12 +0000

    update changes

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,6 @@
+#!/usr/bin/node
+
+const myArr = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+for (let k = 0; k < myArr.length; k++) {
+   console.log(myArr[k]);
+}
---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+
+const process = require('process');
+const firstArg = process.argv[2];
+
+const x = parseInt(firstArg);
+
+if (!isNaN(x)) {
+  for (let k = 0; k < x; k++) {
+    console.log('C is fun');
+  }
+ } else {
+   console.log('Missing number of occurrences');
+}
