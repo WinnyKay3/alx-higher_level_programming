bac693f115a0fe8801eb9c5e29ff11604a8971f6
WinnyKay3 <winffredkalondu@gmail.com>
2023-09-12 02:08:25 +0000

    9-add.js

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -5,6 +5,6 @@ const firstArg = parseInt(process.argv[2]);
 const secondArg = parseInt(process.argv[3]);
 
 function add (a, b) {
-   return a + b;
+  return a + b;
 }
 console.log(add(firstArg, secondArg));
