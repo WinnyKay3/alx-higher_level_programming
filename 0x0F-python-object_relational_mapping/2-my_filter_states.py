c51e3ad302e4fe803d396ffcc1dcc74d8fa8b70d
WinnyKay3 <winffredkalondu@gmail.com>
2023-09-18 15:38:21 +0000

    2-my_filter_states.py

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-#Displays all values in the states table of database hbtn_0e_0_usa
-#Usage: ./my_filter_states.py
+# Displays all values in the states table of database hbtn_0e_0_usa
+# Usage: ./my_filter_states.py
 
 import sys
 import MySQLdb
@@ -8,7 +8,11 @@ import MySQLdb
 if __name__ == "__main__":
     db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
     c = db.cursor()
-    c.execute("SELECT * \
+    c.execute(
+        "SELECT * \
 	         FROM `states` \
-		 WHERE BINARY `name` = '{}'".format(sys.argv[4]))
+		 WHERE BINARY `name` = '{}'".format(
+            sys.argv[4]
+        )
+    )
     [print(state) for state in c.fetchall()]
