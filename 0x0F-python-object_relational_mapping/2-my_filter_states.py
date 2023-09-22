69cb249ec136a3e1002a9ea12ef65ab304ecff41
WinnyKay3 <winffredkalondu@gmail.com>
2023-09-22 12:45:44 +0000

    update task 2 changes

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -6,13 +6,25 @@ import sys
 import MySQLdb
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
-    c = db.cursor()
-    c.execute(
-        "SELECT * \
-		  FROM `statas` \
-		 WHERE BINARY `name` = '{}".format(
-            sys.argv[4]
-        )
-    )
-    [print(state) for state in c.fetchall()]
+    if len(sys.argv) != 5:
+        print("Usage: ./my_filter_states.py <usr> <pwd> <db> <state_name>")
+	sys.exit(1)
+    
+    usr = sys.argv[1]
+    pwd = sys.argv[2]
+    db  = sys.argv[3]
+    state_name = sys.argv[4]
+
+    try:
+        db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+        c = db.cursor()
+        c.execute("SELECT * FROM states WHERE BINARY name = %s")
+
+	results = c.fetchall()
+	for row in results:
+	    print(row)
+	db.close()
+    except MySQLdb.Error as e:
+        print("MySQL Error {}: {}".format(e.args[0], e.args[1]))
+	sys.exit(1)
+        
