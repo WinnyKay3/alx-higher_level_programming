5789e303c27a2ad569c18aace0d229a99c3bda8f
WinnyKay3 <winffredkalondu@gmail.com>
2023-09-30 07:27:16 +0000

    2-my_filter_states.py

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,34 +1,32 @@
 #!/usr/bin/python3
-"""
-Module that connects a python script to a database
-"""
+"""Module that connects a python script to a database"""
 
 import MySQLdb
 from sys import argv
 
-# Connect database using command-line arguments
-my_db = MySQLdb.connect(
-    user=argv[1],
-    passwd=argv[2],
-    db=argv[3],
-    host=("localhost" if len(argv) < 6 else argv[5]),
-)
-
-my_cursor = my_db.cursor()
-
-# Execute a SELECT query to fetch data
-my_cursor.execute(
-    """
-       SELECT * FROM states  WHERE name LIKE BINARY '%{}%'
-       ORDER BY states.id ASC
-     """.format(
-        argv[4]
+if __name__ == "__main__":
+    # Connect database using command-line arguments
+    my_db = MySQLdb.connect(
+        user=argv[1],
+        passwd=argv[2],
+        db=argv[3],
+        host=("localhost" if len(argv) < 6 else argv[5]),
     )
-)
-my_data = my_cursor.fetchall()
 
+    my_cursor = my_db.cursor()
 
-for row in my_data:
-    print(row)
-my_cursor.close()
-my_db.close()
+    # Execute a SELECT query to fetch data
+    my_cursor.execute(
+        """
+           SELECT * FROM states  WHERE name LIKE BINARY '%{}%'
+           ORDER BY states.id ASC
+        """.format(
+            argv[4]
+        )
+    )
+    my_data = my_cursor.fetchall()
+
+    for row in my_data:
+        print(row)
+    my_cursor.close()
+    my_db.close()
